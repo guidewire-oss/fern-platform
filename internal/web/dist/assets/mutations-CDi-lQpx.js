3b98c4ecaede7e1148ff6defba6caebd0b6312b0
cwrm56 <43615527+cwrm56@users.noreply.github.com>
2026-08-20 16:21:18 +0000

    chore(web): rebuild embedded SPA dist for #228 tag-label fix

---
+++ internal/web/dist/assets/mutations-CDi-lQpx.js
@@ -0,0 +1,18 @@
+import{I as n,l as o}from"./index-Clz6hgL8.js";import{u as r}from"./Button-CP-8n8Dp.js";const i=`
+  mutation CreateProject($input: CreateProjectInput!) {
+    createProject(input: $input) {
+      id projectId name
+    }
+  }
+`,c=`
+  mutation UpdateProject($id: ID!, $input: UpdateProjectInput!) {
+    updateProject(id: $id, input: $input) {
+      id projectId name description team defaultBranch repository
+    }
+  }
+`,a=`
+  mutation DeleteProject($id: ID!) {
+    deleteProject(id: $id)
+  }
+`;function d(){const e=n();return r({mutationFn:t=>o(i,{input:t}),onSuccess:()=>e.invalidateQueries({queryKey:["projects"]})})}function j(){const e=n();return r({mutationFn:({id:t,input:u})=>o(c,{id:t,input:u}),onSuccess:()=>e.invalidateQueries({queryKey:["projects"]})})}function m(){const e=n();return r({mutationFn:t=>o(a,{id:t}),onSuccess:()=>e.invalidateQueries({queryKey:["projects"]})})}export{m as a,j as b,d as u};
+//# sourceMappingURL=mutations-CDi-lQpx.js.map
